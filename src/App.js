e74123a2f718b506df4922fb5a2ee23c25fb7c6d
MostafaKhld <mostafa.khaled@zoho.com>
2022-09-28 07:21:44 +0200

    dnd page

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import "./App.css";
 import { Route, Redirect, Switch } from "react-router-dom";
 import ProtectedRoute from "./components/protectedRoute";
 import SignIn from "./components/login";
+import Todo from "./components/todo";
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       <Switch>
         {/* <ProtectedRoute path="/movies/:id" component={MovieForm} /> */}
-        <Route path="/" component={SignIn} />
+        {/* <Route path="/" component={SignIn} /> */}
+
+        <Route path="/" component={Todo} />
       </Switch>
     </div>
   );
